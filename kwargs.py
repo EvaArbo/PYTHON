b738518c67baaff21b0681e57ce4069351958d69
EvaArbo <evelynearbogusti@gmail.com>
2025-08-05 11:29:25 +0300

    mixed args and kwargs

--- kwargs.py
+++ kwargs.py
@@ -9,4 +9,21 @@ def Hello(name,Nationality):
     print("Name is", name)
     print("From the", Nationality)
 Hello(Nationality="STATE", name="Eva")
-          
+#KWARGS PROFILE
+def employee(**kwargs):
+    print(kwargs)
+employee(name="John", age=30, position="Developer")
+employee(name="Alice", age=25, position="Designer", country="USA")
+#employee(Bob, age=40)  # This will raise an error because 'Bob' is not a keyword argument
+#kwargs allows you to pass a variable number of keyword arguments to a function
+                                                                            
+def mixed(*args, **kwargs):
+    print ("Positional arguments:", args)
+    print ("Keyword arguments:", kwargs)
+mixed(1, 2, 3, name="Alice", age=30)
+# This will print:
+# Positional arguments: (1, 2, 3)
+# Keyword arguments: {'name': 'Alice', 'age': 30}
+# You can also pass arguments as a list
+   
+   
