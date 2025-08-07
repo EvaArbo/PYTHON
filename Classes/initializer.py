be0c8377cdfa11b5aedfe930b2bd8a1582b62c77
EvaArbo <evelynearbogusti@gmail.com>
2025-08-07 10:17:52 +0300

    introduced classes and initializers

---
+++ Classes/initializer.py
@@ -0,0 +1,8 @@
+class Human():
+    def __init__(sel):
+        print("Human class initialized")
+    def  another_method(self):
+        print("This is another method in the Human class")
+
+Evah= Human()
+Evah.another_method()
---
+++ Classes/intro.py
@@ -0,0 +1,19 @@
+class student():
+    name= "Evah"
+    age= 21
+    email= "evelynearbogasti@gmail.com"
+    phone= "0700000000"
+
+student1= student()
+student2= student()
+print("Student 1 details:")
+print(student1.name)
+print(student1.age)
+print(student1.email)
+print(student1.phone)
+
+print("Student 2 details:")
+print(student2.name)
+print(student2.age)
+print(student2.email)
+print(student2.phone)
