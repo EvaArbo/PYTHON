32f399c6195edf1739f5c7b7657a36d044cb907d
EvaArbo <evelynearbogusti@gmail.com>
2025-08-07 10:27:01 +0300

    updated the initializer

--- Classes/initializer.py
+++ Classes/initializer.py
@@ -1,8 +1,12 @@
 class Human():
-    def __init__(sel):
+    def __init__(self):
         print("Human class initialized")
     def  another_method(self):
         print("This is another method in the Human class")
 
 Evah= Human()
 Evah.another_method()
+#initializer is same as constructor in javascript
+#initializer is called when an object is created
+#The purpose of the initializer is to initialize the attributes of the class
+
