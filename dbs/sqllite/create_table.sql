322891535bcd6261ada12f7c018f8b20cde597b4
EvaArbo <evelynearbogusti@gmail.com>
2025-08-15 09:45:50 +0300

    added data

--- dbs/sqllite/create_table.sql
+++ dbs/sqllite/create_table.sql
@@ -9,6 +9,11 @@ CREATE TABLE parent(
     id SERIAL PRIMARY KEY
 );
 
+-- INSERT INTO student (name,email,dob,phone,marks)
+-- VALUES ('alice','alice@gmail.com','2002-05-14',254732323,88.2);
+
+-- SELECT * FROM student
+
 
 
 
